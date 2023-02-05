df4e8c63dbdc03fc131f763994cc60a9c146b2e3
hamatiq <thabit.hamza@gmail.com>
2023-02-05 15:17:39 -0600

    update

--- Todo.cs
+++ Todo.cs
@@ -1,9 +1,16 @@
 ﻿namespace minTodoApi
 {
-    class Todo
+    public class Todo
     {
         public int Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+        public Todo() { }
+        public Todo(Todo todoItem)
+        {
+            (Id, Name, IsComplete, Secret) = (todoItem.Id, todoItem.Name, todoItem.IsComplete, todoItem.Secret);
+
+        }
     }
 }
